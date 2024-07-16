7706b57f88a968744be3cae5f1402e5e176794b1
rshdwantstocode <rushseed02@gmail.com>
2024-07-16 19:28:23 +0800

    Added public class to call the output css and started with the design and layout for the note

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index");
 });
 
 app.listen(port, ()=>{
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/first.html",
-    './views/index.ejs',
+  content: [ "./public/src/**/*.{html,js,css}",
+        "./views/**/*.ejs",
   ],
   theme: {
     extend: {},
