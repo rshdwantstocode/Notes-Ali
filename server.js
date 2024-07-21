b75601654b1d429276f41c16a319f108669dbfed
rshdwantstocode <rushseed02@gmail.com>
2024-07-21 16:09:00 +0800

    Installed pg and made an input to the database, also connected to pgAdmin, made the textarea in add note required

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import axios from 'axios';
 import bodyParser from 'body-parser';
+import pg from 'pg';
 
 const app  = express();
 const port = 3000;
+
+const db = new pg.Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'Notes',
+    password: 'alipostgres',
+    port: 5432,// default PostgreSQL port
+});
+
+db.connect()
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +26,20 @@ app.get("/", (req, res) => {
 
 app.get("/addNotes", (req, res) => {
     res.render('Add-Notes');
+});
+
+app.post("/postNotes", async (req,res) => {
+    const {title, content} = req.body;
+    try {
+        const result = await db.query(
+            "INSERT INTO notes (Title, Content) VALUES ($1, $2)",
+            [title, content]
+        );
+        console.log("Successfully Added");
+        res.redirect("/")
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.listen(port, ()=>{
