fbddfd458a00f3bbddef4ad8cdd772c83dce45f5
rshdwantstocode <rushseed02@gmail.com>
2024-07-28 10:03:17 +0800

    deleted some commented codes and moved the func.js into public folder

--- func.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const notes = document.querySelectorAll(".note");
-    notes.forEach(note => {
-        note.addEventListener("mouseover", () => {
-            note.innerHTML = "Hello"
-        });
-    });
-});
---
+++ public/func.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const notes = document.querySelector(".note");
+    // notes.forEach(note => {
+        console.log('Hello');
+    // })
+});
